6d3e4f6a3fcb7e15e006df0d3bd8bdcfdfc7b693
Shane Freeder <theboyetronic@gmail.com>
2018-03-30 20:12:25 +0100

    Cleanup MOTD handling

---
+++ src/main/kotlin/im/alphhe/alphheimplugin/components/motd/commands/MotdCommand.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Shane Freeder
+ * Unauthorized copying of this file is not permitted!
+ * Written by Shane Freeder - 2018.
+ */
+
+package im.alphhe.alphheimplugin.components.motd.commands
+
+import co.aikar.commands.annotation.Default
+import im.alphhe.alphheimplugin.commands.AlphheimCommand
+import im.alphhe.alphheimplugin.components.motd.MotdHandler
+import org.bukkit.command.CommandSender
+
+class MotdCommand(val handler: MotdHandler) : AlphheimCommand(handler.plugin, "motd") {
+
+    @Default
+    fun onCommand(sender: CommandSender): Unit {
+        handler.listener.sendMotd(sender)
+    }
+
+
+}
