9391e5eee3ab1a72a95e3e11fecd2adc882bd820
Shane Freeder <theboyetronic@gmail.com>
2018-04-09 01:30:53 +0100

    Remove some debug statements

--- src/main/kotlin/im/alphhe/alphheimplugin/data/AlphheimUser.kt
+++ src/main/kotlin/im/alphhe/alphheimplugin/data/AlphheimUser.kt
@@ -32,44 +32,29 @@ class AlphheimUser(val uuid: UUID, @Suppress("UNUSED_PARAMETER") isNPC: Boolean 
     }
 
     init {
-        println("creating user...")
-
         MySQL.getConnection().use { connection ->
-            println("open conn")
             val prepareStatement = connection.prepareStatement("SELECT PLAYER_ID FROM player_data WHERE PLAYER_UUID = ?");
-            println("got prep")
             prepareStatement.use { statement ->
-                println("using prep")
                 statement.setString(1, uuid.toString())
                 if (statement.execute()) {
-                    println("using prep1")
                     val resultSet = statement.resultSet
 
                     resultSet.use { rs ->
-                        println("rs1 use")
                         if (rs.next()) {
-                            println("rs next")
                             userID = rs.getInt(rs.findColumn("PLAYER_ID"))
                         }
-                        println("finuse")
                     }
                 }
             }
-
-            println("check + $userID")
-
         }
         if (userID == -1) {
 
             MySQL.getConnection().use { connection ->
                 val statement1 = connection.prepareStatement("INSERT INTO player_data (PLAYER_UUID) VALUES (?)", Statement.RETURN_GENERATED_KEYS)
-                println("stmt2")
                 statement1.use { insertStatement ->
 
-                    println("2nd statement")
                     insertStatement.setString(1, uuid.toString())
                     insertStatement.executeUpdate()
-                    println("sount")
 
                     insertStatement.use {
                         it.generatedKeys.use {
