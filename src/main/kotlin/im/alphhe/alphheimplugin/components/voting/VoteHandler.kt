a8c97abb450c6a7963cf835cbf1cb32105c8a5b7
Shane Freeder <theboyetronic@gmail.com>
2018-05-05 23:43:07 +0100

    Start work towards VoteHandler

---
+++ src/main/kotlin/im/alphhe/alphheimplugin/components/voting/VoteHandler.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Alphheim
+ * Unauthorized copying of this file is not permitted!
+ * Written by Shane Freeder - 2018.
+ */
+
+package im.alphhe.alphheimplugin.components.voting
+
+import im.alphhe.alphheimplugin.AlphheimCore
+
+class VoteHandler(private var plugin: AlphheimCore) {
+
+    init {
+
+    }
+
+}
