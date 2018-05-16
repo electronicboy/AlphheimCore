6be3ace57ead2f53babf6cce4ca6609e5e4e38c2
Shane Freeder <theboyetronic@gmail.com>
2018-05-16 20:06:34 +0100

    add saddle recipe

--- src/main/kotlin/im/alphhe/alphheimplugin/components/diversions/FunHandler.kt
+++ src/main/kotlin/im/alphhe/alphheimplugin/components/diversions/FunHandler.kt
@@ -9,12 +9,27 @@ package im.alphhe.alphheimplugin.components.diversions
 import im.alphhe.alphheimplugin.AlphheimCore
 import im.alphhe.alphheimplugin.components.diversions.commands.CommandRoulette
 import im.alphhe.alphheimplugin.components.diversions.listeners.FurnaceListener
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.ShapedRecipe
 
 class FunHandler(var plugin: AlphheimCore) {
 
     init {
         plugin.server.pluginManager.registerEvents(FurnaceListener(), plugin)
         plugin.commandManager.registerCommand(CommandRoulette(plugin))
+
+        try { registerRecipes() } catch (ex: Exception) {}
+
+    }
+
+    private fun registerRecipes() {
+        val saddleRecipe = ShapedRecipe(ItemStack(Material.SADDLE))
+        saddleRecipe.shape("sll", "lxx", "lll")
+        saddleRecipe.setIngredient('s', Material.STRING)
+        saddleRecipe.setIngredient('l', Material.LEATHER)
+        saddleRecipe.setIngredient('x', Material.STICK)
+        plugin.server.addRecipe(saddleRecipe)
     }
 
 }
