e6b09793ec2f5d3b4205f4e35c665d27b428355e
Shane Freeder <theboyetronic@gmail.com>
2018-05-18 23:19:56 +0100

    change debug message

--- src/main/kotlin/im/alphhe/alphheimplugin/components/racial/RacialHandler.kt
+++ src/main/kotlin/im/alphhe/alphheimplugin/components/racial/RacialHandler.kt
@@ -91,7 +91,7 @@ class RacialHandler(private val plugin: AlphheimCore) {
                     }
 
                     if (potionEffect != null && potionEffect.amplifier >= entry.value) {
-                        println("removing ${entry.key} from ${player.name}  #94")
+                        println("removing ${entry.key} from ${player.name}  #94 ${potionEffect.amplifier} >= ${entry.value}")
                         player.addPotionEffect(PotionEffect(entry.key, Int.MAX_VALUE, entry.value, true, false), true)
                     }
                 }
