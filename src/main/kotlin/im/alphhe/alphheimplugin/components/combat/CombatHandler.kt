f82b9dc01e6f438bd24c1b57f185cbdd8e1728c6
Shane Freeder <theboyetronic@gmail.com>
2018-08-25 04:09:11 +0100

    Remove elytra recipe

--- src/main/kotlin/im/alphhe/alphheimplugin/components/combat/CombatHandler.kt
+++ src/main/kotlin/im/alphhe/alphheimplugin/components/combat/CombatHandler.kt
@@ -10,13 +10,28 @@ import im.alphhe.alphheimplugin.AlphheimCore
 import im.alphhe.alphheimplugin.components.AbstractHandler
 import im.alphhe.alphheimplugin.components.combat.listeners.CombatListener
 import im.alphhe.alphheimplugin.components.combat.listeners.PotionListener
+import org.bukkit.Bukkit
+import org.bukkit.Keyed
+import org.bukkit.Material
 
 class CombatHandler(plugin: AlphheimCore) : AbstractHandler(plugin) {
+    val toRemove: Set<Material>
 
 
     init {
         CombatListener(plugin)
         PotionListener(plugin)
+
+        toRemove = setOf(Material.ELYTRA)
+
+        val recipeIterator = Bukkit.getServer().recipeIterator();
+        while (recipeIterator.hasNext()) {
+            val recipe = recipeIterator.next()
+            if (toRemove.contains(recipe.result.type)) {
+                recipeIterator.remove();
+            }
+        }
+
     }
 
 
