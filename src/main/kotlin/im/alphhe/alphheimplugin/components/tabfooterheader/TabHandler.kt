e3eedc2701dc42cb1d01fd3a4f69d779ab2f4ad9
Shane Freeder <theboyetronic@gmail.com>
2018-08-30 04:11:00 +0100

    Test header/footer legacy method, TextComponent appears to break the tab...

--- src/main/kotlin/im/alphhe/alphheimplugin/components/tabfooterheader/TabHandler.kt
+++ src/main/kotlin/im/alphhe/alphheimplugin/components/tabfooterheader/TabHandler.kt
@@ -78,7 +78,8 @@ class TabHandler(plugin: AlphheimCore) : AbstractHandler(plugin) {
         val headerComponent = TextComponent.fromLegacyText(header)
         val footerComponent = TextComponent.fromLegacyText(footer)
 
-        player.setPlayerListHeaderFooter(headerComponent, footerComponent)
+        //player.setPlayerListHeaderFooter(headerComponent, footerComponent)
+        player.setPlayerListHeaderFooter(header, footer)
 
     }
 
