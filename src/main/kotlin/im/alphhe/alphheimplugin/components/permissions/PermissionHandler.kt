74510595a405e8d067fbb7b04ccc76cbd26a029d
Shane Freeder <theboyetronic@gmail.com>
2018-12-03 04:06:57 +0000

    Attempt to fix tier info

--- src/main/kotlin/im/alphhe/alphheimplugin/components/permissions/PermissionHandler.kt
+++ src/main/kotlin/im/alphhe/alphheimplugin/components/permissions/PermissionHandler.kt
@@ -109,19 +109,25 @@ class PermissionHandler(plugin: EladriaCore) : AbstractHandler(plugin) {
             DonorTier.values().reversedArray().forEach { tier ->
                 run {
                     if (tier != DonorTier.NONE) {
-                        val result = user.hasPermission(plugin.luckPermsApi.nodeFactory.makeGroupNode(getGroup(tier.name.toLowerCase())!!).build())
-                        if (result.asBoolean()) {
+                        user.ownNodes.forEach { node ->
+                            run {
+                                if (node.isGroupNode && node.groupName.equals(tier.name, true)) {
 
-                            MySQL.getConnection().use { conn -> {
-                                conn.prepareStatement("UPDATE player_data SET PLAYER_DONATION_TIER = ? WHERE PLAYER_UUID = ?").use { ps -> {
-                                    ps.setInt(1, tier.level)
-                                    ps.setString(2, uuid.toString())
-                                    if (ps.executeUpdate() == 0) {
-                                        plugin.logger.warning("FAILED TO TRANSLATE DATA FOR: $uuid")
+                                    MySQL.getConnection().use { conn ->
+                                        {
+                                            conn.prepareStatement("UPDATE player_data SET PLAYER_DONATION_TIER = ? WHERE PLAYER_UUID = ?").use { ps ->
+                                                {
+                                                    ps.setInt(1, tier.level)
+                                                    ps.setString(2, uuid.toString())
+                                                    if (ps.executeUpdate() == 0) {
+                                                        plugin.logger.warning("FAILED TO TRANSLATE DATA FOR: $uuid")
+                                                    }
+                                                }
+                                            }
+                                        }
                                     }
-                                }}
-                            }}
-
+                                }
+                            }
                         }
                     }
                 }
